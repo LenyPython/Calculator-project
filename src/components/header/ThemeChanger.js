2dc0a9a5ae5163170ccac6923278db67ddf4ba39
LenyPython <buniadyserv@gmail.com>
2021-08-09 16:15:12 +0200

    compolete theme changer component. onClick changes body data-theme to specific theme name

--- src/components/header/ThemeChanger.js
+++ src/components/header/ThemeChanger.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './themechanger.css'
 
 const ThemeChanger = () => {
+
+  const handleClick = event => {
+    let theme = event.target.innerText.toString()
+    document.body.dataset.theme = 'theme-' + theme
+    let slider = document.querySelector('#slider')
+    slider.className = ''
+
+    if (theme === '2') slider.className = 'pos-2'
+    if (theme === '3') slider.className = 'pos-3'
+  }
   return (
     <div id='changer'>
       <p>theme</p>
       <div className='themes'>
+        <div>
+          <button onClick={handleClick} className='theme-btn'>1</button>
+          <button onClick={handleClick} className='theme-btn'>2</button>
+          <button onClick={handleClick} className='theme-btn'>3</button>
+        </div>
+        <span id='slider'></span>
       </div>
     </div>
   )
--- src/components/keys/Keys.js
+++ src/components/keys/Keys.js
@@ -80,7 +80,7 @@ const Keys = ({calcMemo: {value, setValue, prevValue, setPrevValue}}) => {
   }
 
   return (
-    <div id='keys'>
+    <div id='keys' theme='theme2'>
       <button id='one' onClick={numberClick}>1</button>
       <button id='two' onClick={numberClick}>2</button>
       <button id='three' onClick={numberClick}>3</button>
