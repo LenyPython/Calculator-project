4fdcffbdb61af0493ab2e48ab189fd88b77337b0
LenyPython <buniadyserv@gmail.com>
2021-09-14 18:50:07 +0200

    Writing simple integration tests for calc

---
+++ src/__test__/App.test.js
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/react'
+
+
+describe('Simple App integration tests', () => {
+  describe('Single operation tests', () => {
+    test('1 + 2 = 3', () => {
+
+    })
+  })
+})
